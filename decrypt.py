1d600fb32f3c2911163a948258d4e2fa610a7c19
softtrick <159680388+softtrick@users.noreply.github.com>
2024-03-21 08:48:41 +1000

    Update decrypt.py
    Updated to support encryption now using the individual digits of the unicode point values of letters used in a key i.e. A would be interpreted as '6, 5' instead of '65'

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,5 @@
 # Decrypt
 
-
 def main():
     # Get cyphertext user input
     cyphertext = ""
@@ -12,26 +11,32 @@ def main():
     while not key:
         key = input("\nEnter key:\n")
 
-    # Convert nun-numeric characters to unicode code points
-    key = [int(ord(char)) if not char.isdigit() else int(char) for char in key]
-
-    # Decrypt plaintext and print result
+    # Decrypt cyphertext using key
     plaintext = decrypt(cyphertext, key)
-    print(f"\nPlaintext:\n{plaintext}\n")
 
+    # Print result
+    print(f"\nPlaintext:\n{plaintext}\n")
     input("Press Enter to close...")
     
 # Define decrypt function
 def decrypt(cyphertext, key):
+    
+    # Convert nun-numeric characters to unicode code points
+    key = "".join((str(ord(char)) if not char.isdigit() else char for char in key))
+
+    # Convert each char in str key to an int in a list called key
+    key = [int(char) for char in key]
+
+    # Define variables for plaintext string and n for iterations of the loop
     plaintext = ""
     n = 0
-
     for char in cyphertext:
 
         # If there are no more indexes in key, go back to start
         if n == len(key):
             n = 0
-            
+
+        # Decrypt cyphertext using key
         plaintext += chr(ord(char) - key[n])
 
         # Move to the next index in key
