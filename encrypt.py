68c852f0bcb87d13f273b6ddea71bf36ab6c27a0
softtrick <159680388+softtrick@users.noreply.github.com>
2024-03-21 08:41:26 +1000

    Update encrypt.py
    Created function generate_key
    
    encryption now uses the individual digits of the unicode point values of letters used in a key i.e. A would be interpreted as '6, 5' instead of '65'

--- encrypt.py
+++ encrypt.py
@@ -4,45 +4,50 @@ from random import randint
 
 def main():
     # Get plaintext user input
-    plaintext = input("Enter plaintext:\n")
+    plaintext = input("Enter plaintext:\n").strip()
 
     # Get key user input
     key = ""
     while not key:
-        key = input("\nEnter key\n(type 'n' to generate a random key):\n")
+        key = input("\nEnter key\n(type 'n' to generate a random key):\n").strip()
 
     # If key is 'n', generate a random key
     if key == "n":
-
         # if plaintext is empty generate set key length to 10
-        length = len(plaintext) if plaintext else 10
+        generate_key(len(plaintext) if plaintext else 10)
 
-        # Generate random key
-        numeric_key = [randint(1, 9) for _ in range(length)]
-        key = "".join(map(str, numeric_key))
-        
-    # Convert nun-numeric characters to unicode code points
-    else:
-        numeric_key = [ord(char) if not char.isdigit() else int(char) for char in key]
-        
-    cyphertext = encrypt(plaintext, numeric_key)
-    print(f"\nKey:\n{key}\n")
-    print(f"Cyphertext:\n{cyphertext}\n")
-    
+    # Encrypt plaintext using key
+    cyphertext = encrypt(plaintext, key)
+
+    # Print result and key
+    print(f"\nKey:\n{key}\nCyphertext:\n{cyphertext}\n")
     input("Press Enter to close...")
+
+# Define generate key function
+def generate_key(length):
+    
+    # Generate random key
+    return ''.join(str(randint(0, 9)) for _ in range(length))
 
 # Define encrypt function
 def encrypt(plaintext, key):
+
+    # Convert nun-numeric characters to unicode code points
+    key = "".join((str(ord(char)) if not char.isdigit() else char for char in key))
+
+    # Convert each char in str key to an int in a list called key
+    key = [int(char) for char in key]
+
+    # Define variables for cyphertext string and n for iterations of the loop
     cyphertext = ""
     n = 0
-    
-    # Convert each char in str key to an int in a list called key
-    key = [int(n) for n in key]
     for char in plaintext:
         
         # If there are no more indexes in key, go back to start
         if n == len(key):
             n = 0
+            
+        # Encrypt plaintext using key
         cyphertext += chr(ord(char) + key[n])
         
         # Move to the next index in key
